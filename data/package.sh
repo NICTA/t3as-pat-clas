55d404f06eb675121b6ec01a473ee3f584b76790
nbacon <neil.bacon@nicta.com.au>
2014-05-07 13:05:44 +1000

    add deployment instructions

--- data/package.sh
+++ data/package.sh
@@ -1,5 +1,38 @@
 #! /bin/bash
 
+# # Steps to release and deploy a new version to EC2
+# # ================================================
+
+# # update poms for release
+# mvn versions:set -DnewVersion=1.0.3
+# mvn versions:commit
+# git commit -a -m 'update version for release'
+# git push
+# 
+# # create a release tag
+# git tag -a pat-clas-1.0.3 -m 'tag for release'
+# git push origin pat-clas-1.0.3
+# 
+# # do a clean build
+# mvn clean
+# mvn
+# 
+# # create packages to deploy on ec2
+# cd data
+# # optionally comment out running pat-clas-parse-*.one-jar.jar near the top if there is no data change
+# ./package.sh
+# 
+# # upload packages to s3
+# Log in to EC2, S3 bucket t3as-webapps
+# upload the 3 *.tar.gz files created by package.sh
+# 
+# Do Mats' magic deplopyment stuff.
+# 
+# # update poms for ongoing dev
+# mvn versions:set -DnewVersion=1.0.4-SNAPSHOT
+# mvn versions:commit
+# git commit -a -m 'update version for dev'
+
 set -e
 
 java -jar ../pat-clas-parse/target/pat-clas-parse-*.one-jar.jar
